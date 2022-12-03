56597c828961385e3ec415eb7b4ed249f8afa554
Vilberg <vilbergs@gmail.com>
2022-12-03 07:30:55 +0100

    Do all stling in main

--- src/main.rs
+++ src/main.rs
@@ -9,38 +9,39 @@ fn main() {
         "./input.txt";
 
     let input = fs::read_to_string(filename).expect("Could not read input");
+    let input_part_2 = input.clone(); // Cloning input here so it doesn't affect benchmarks
 
-    part1(input.clone());
-    part2(input);
-}
-
-fn part1(input: String) {
     println!("=======================");
     println!("Running Part 1");
     println!("=======================");
 
     let now = Instant::now();
     
-    
-    // Implement
-    
+    part1(input);
+
     let elapsed = now.elapsed();
     println!("-----------------------");
     println!("Part 1 ran in: {:.2?}", elapsed);
     println!("-----------------------");
-}
 
-fn part2(input: String) {
     println!("=======================");
     println!("Running Part 2");
     println!("=======================");
     
     let now = Instant::now();
 
-    // Implement
+    part2(input_part_2);
 
     let elapsed = now.elapsed();
     println!("-----------------------");
     println!("Part 2 ran in: {:.2?}", elapsed);
     println!("-----------------------");
+}
+
+fn part1(input: String) {
+    todo!("Implement part 1");
+}
+
+fn part2(input: String) {
+    todo!("Implement part 1");
 }
